39dc0df37b132d227fcbbb23611924682350118c
Kukyeon <rnrdus2000@naver.com>
2025-07-30 09:04:38 +0900

    수정

--- src/ex_20250729/Ex_5.java
+++ src/ex_20250729/Ex_5.java
@@ -5,13 +5,20 @@ public class Ex_5 {
 			// TODO Auto-generated method stub
 			//5. 다음 문자열에서 알파벳의 개수를 출력하는 프로그램을 작성하시오. (hint : 아스키 코드값 활용)
 			//"Hello123, Java456"
-			char ch = '1';
+			String str = "Hello123, Java456";
+			int count = 0;
 			
-			if (ch >=65 && ch <= 90) {//65 -> 대문자 A 아스키코드값, 90 -> 대문자 Z 아스키코드값
-				System.out.println(ch + "는 영문자 대문자 입니다.");
-			} else if (ch >= 97 && ch <= 122 ){
-				System.out.println(ch + "는 영문자 소문자 입니다.");
-			}
-		}
+			
+			for(int i = 0 ; i < str.length() ; i++) {
+				char ch = str.charAt(i);
+				
+				
+				//65 -> 대문자 A 아스키코드값, 90 -> 대문자 Z 아스키코드값
+			if ((ch >=65 && ch <= 90) || (ch >= 97 && ch <= 122 )) {
+				
+				count++;
+			} 
+		}		System.out.println(count);
 
 	}
+}
